a4e97d268191d5ce9d305eea480d46ac9dc5d91d
方正 <215566435@qq.com>
2018-02-17 11:38:58 +1300

    修复 ts bug

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { DragactProps } from './src/lib/dragact';
 
-declare class Dragact extends React.Component<DragactProps, {}> {
+export declare class Dragact extends React.Component<DragactProps, {}> {
 }
 
-export = Dragact;
+
 export as namespace Dragact;
