93bbc067bd73151e01dbf3e6b43f99263fa65fd7
Z F <215566435@qq.com>
2018-02-20 14:36:21 +1300

    去除没用的提示

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 var Dragact = require('./dist/lib/dragact').Dragact;
 
-// Previous versions of this lib exported <Draggable> as the root export. As to not break
-// them, or TypeScript, we export *both* as the root and as 'default'.
-// See https://github.com/mzabriskie/react-draggable/pull/254
-// and https://github.com/mzabriskie/react-draggable/issues/266
+
 exports.Dragact = Dragact;
